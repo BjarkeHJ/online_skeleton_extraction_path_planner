52a6912130a58765087f6f4e0a417386e1dded14
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-08-26 16:09:20 +0200

    This is good

--- src/osep/launch/osep.launch.py
+++ src/osep/launch/osep.launch.py
@@ -36,77 +36,77 @@ def generate_launch_description():
                 'interpolation_distance': INTERPOLATION_DISTANCE,
                 'max_speed': 15.0,
                 'inspection_speed': INSPECTION_SPEED,
-                'max_yaw_to_velocity_angle_deg': 100.0,
+                'max_yaw_to_velocity_angle_deg': 120.0,
                 'frequency': 50,
                 'sharp_turn_thresh_deg': 30.0,
             }]
         ),
-    #     Node(
-    #         package="osep_2d_map",
-    #         executable="costmap_2d_node",
-    #         name="costmap_2d_node",
-    #         output="screen",
-    #         parameters=[
-    #             {"resolution": 1.0},
-    #             {"free_center_radius": 5.0},
-    #             {"local_map_size": 400.0},
-    #             {"global_map_size": 1600.0},
-    #             {"frame_id": FRAME_ID},
-    #             {"safety_distance": SAFETY_DISTANCE},
-    #             {"costmap_topic": TOPIC_NAMES["COSTMAP"]},
-    #         ]
-    #     ),
-    #     Node(
-    #         package="osep_2d_map",
-    #         executable="path_interpolator_node",
-    #         name="planner",
-    #         output="screen",
-    #         parameters=[
-    #             {"frame_id": FRAME_ID},
-    #             {"interpolation_distance": INTERPOLATION_DISTANCE},
-    #             {"costmap_topic": TOPIC_NAMES["COSTMAP"]},
-    #             {"viewpoints_topic": TOPIC_NAMES["VIEWPOINTS"]},
-    #             {"path_topic": TOPIC_NAMES["PATH"]},
-    #             {"ground_truth_topic": TOPIC_NAMES["GROUND_TRUTH"]},
-    #             {"viewpoints_adjusted_topic": TOPIC_NAMES["VIEWPOINTS_ADJUSTED"]},
-    #             {"ground_truth_update_interval": 8000},
-    #             {"safety_distance": SAFETY_DISTANCE},
-    #         ]
-    #     ),
-    #     Node(
-    #         package="osep_skeleton_decomp",
-    #         executable="osep_rosa",
-    #         name="RosaNode",
-    #         output="screen",
-    #         parameters=[
+        Node(
+            package="osep_2d_map",
+            executable="costmap_2d_node",
+            name="costmap_2d_node",
+            output="screen",
+            parameters=[
+                {"resolution": 1.0},
+                {"free_center_radius": 5.0},
+                {"local_map_size": 400.0},
+                {"global_map_size": 1600.0},
+                {"frame_id": FRAME_ID},
+                {"safety_distance": SAFETY_DISTANCE},
+                {"costmap_topic": TOPIC_NAMES["COSTMAP"]},
+            ]
+        ),
+        Node(
+            package="osep_2d_map",
+            executable="path_interpolator_node",
+            name="planner",
+            output="screen",
+            parameters=[
+                {"frame_id": FRAME_ID},
+                {"interpolation_distance": INTERPOLATION_DISTANCE},
+                {"costmap_topic": TOPIC_NAMES["COSTMAP"]},
+                {"viewpoints_topic": TOPIC_NAMES["VIEWPOINTS"]},
+                {"path_topic": TOPIC_NAMES["PATH"]},
+                {"ground_truth_topic": TOPIC_NAMES["GROUND_TRUTH"]},
+                {"viewpoints_adjusted_topic": TOPIC_NAMES["VIEWPOINTS_ADJUSTED"]},
+                {"ground_truth_update_interval": 8000},
+                {"safety_distance": SAFETY_DISTANCE},
+            ]
+        ),
+        Node(
+            package="osep_skeleton_decomp",
+            executable="osep_rosa",
+            name="RosaNode",
+            output="screen",
+            parameters=[
 
-    #         ]
-    #     ),
-    #     Node(
-    #         package="osep_skeleton_decomp",
-    #         executable="osep_gskel",
-    #         name="GSkelNode",
-    #         output="screen",
-    #         parameters=[
+            ]
+        ),
+        Node(
+            package="osep_skeleton_decomp",
+            executable="osep_gskel",
+            name="GSkelNode",
+            output="screen",
+            parameters=[
 
-    #         ]
-    #     ),
-    #     Node(
-    #         package="osep_planning",
-    #         executable="osep_planner",
-    #         name="PlannerNode",
-    #         output="screen",
-    #         parameters=[
+            ]
+        ),
+        Node(
+            package="osep_planning",
+            executable="osep_planner",
+            name="PlannerNode",
+            output="screen",
+            parameters=[
 
-    #         ]
-    #     ),
-    #     Node(
-    #         package="osep_planning",
-    #         executable="osep_viewpoint_manager",
-    #         name="ViewpointNode",
-    #         output="screen",
-    #         parameters=[
+            ]
+        ),
+        Node(
+            package="osep_planning",
+            executable="osep_viewpoint_manager",
+            name="ViewpointNode",
+            output="screen",
+            parameters=[
 
-    #         ]
-    #     )
-    # ])
+            ]
+        )
+    ])
