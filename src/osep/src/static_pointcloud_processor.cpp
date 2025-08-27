daf5493d46cc5b8880332b9dc6cb8e900d09e93c
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-08-27 16:29:23 +0200

    upsampling works

--- src/osep/src/static_pointcloud_processor.cpp
+++ src/osep/src/static_pointcloud_processor.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <Eigen/Core>
 
+
 class StaticPointcloudPostprocessNode : public rclcpp::Node
 {
 public:
@@ -16,10 +17,12 @@ public:
         this->declare_parameter<std::string>("static_input_topic", "osep/tsdf/static_pointcloud");
         this->declare_parameter<std::string>("output_topic", "osep/tsdf/upsampled_static_pointcloud");
         this->declare_parameter<double>("voxel_size", 1.0);  // 1.0 m by default
+        this->declare_parameter<int>("upsample_N", 3);       // Default upsampling factor
 
         static_input_topic_ = this->get_parameter("static_input_topic").as_string();
         output_topic_ = this->get_parameter("output_topic").as_string();
         voxel_size_ = this->get_parameter("voxel_size").as_double();
+        upsample_N_ = this->get_parameter("upsample_N").as_int();
 
         sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
             static_input_topic_, 1,
@@ -31,12 +34,38 @@ private:
     std::string static_input_topic_;
     std::string output_topic_;
     double voxel_size_;
+    int upsample_N_;
 
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_;
 
     size_t last_point_count_ = 0;
     sensor_msgs::msg::PointCloud2 last_msg_;
+
+    // Upsampling function
+    std::vector<pcl::PointXYZ, Eigen::aligned_allocator<pcl::PointXYZ>>
+    upsample_voxels(const std::vector<pcl::PointXYZ, Eigen::aligned_allocator<pcl::PointXYZ>>& voxelCenters, double voxel_size, int N)
+    {
+        std::vector<pcl::PointXYZ, Eigen::aligned_allocator<pcl::PointXYZ>> upsampled_points;
+        double sub_step = voxel_size / N;
+        for (const auto& center : voxelCenters) {
+            double start_x = center.x - voxel_size / 2.0 + sub_step / 2.0;
+            double start_y = center.y - voxel_size / 2.0 + sub_step / 2.0;
+            double start_z = center.z - voxel_size / 2.0 + sub_step / 2.0;
+            for (int ix = 0; ix < N; ++ix) {
+                for (int iy = 0; iy < N; ++iy) {
+                    for (int iz = 0; iz < N; ++iz) {
+                        pcl::PointXYZ pt;
+                        pt.x = start_x + ix * sub_step;
+                        pt.y = start_y + iy * sub_step;
+                        pt.z = start_z + iz * sub_step;
+                        upsampled_points.push_back(pt);
+                    }
+                }
+            }
+        }
+        return upsampled_points;
+    }
 
     void callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
     {
@@ -61,25 +90,8 @@ private:
         std::vector<pcl::PointXYZ, Eigen::aligned_allocator<pcl::PointXYZ>> voxelCenters;
         octree.getOccupiedVoxelCenters(voxelCenters);
 
-        int N = 3;
-        double sub_step = voxel_size_ / N;
-        std::vector<pcl::PointXYZ, Eigen::aligned_allocator<pcl::PointXYZ>> upsampled_points;
-        for (const auto& center : voxelCenters) {
-            double start_x = center.x - voxel_size_ / 2.0 + sub_step / 2.0;
-            double start_y = center.y - voxel_size_ / 2.0 + sub_step / 2.0;
-            double start_z = center.z - voxel_size_ / 2.0 + sub_step / 2.0;
-            for (int ix = 0; ix < N; ++ix) {
-                for (int iy = 0; iy < N; ++iy) {
-                    for (int iz = 0; iz < N; ++iz) {
-                        pcl::PointXYZ pt;
-                        pt.x = start_x + ix * sub_step;
-                        pt.y = start_y + iy * sub_step;
-                        pt.z = start_z + iz * sub_step;
-                        upsampled_points.push_back(pt);
-                    }
-                }
-            }
-        }
+        // Upsample using the function
+        auto upsampled_points = upsample_voxels(voxelCenters, voxel_size_, upsample_N_);
 
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_upsampled(new pcl::PointCloud<pcl::PointXYZ>);
         cloud_upsampled->points = upsampled_points;
