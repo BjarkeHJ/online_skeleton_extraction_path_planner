03d45f90256e5eeb2434ceb4022f065bb39ace47
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-08-28 18:01:27 +0200

    prettier

--- src/osep/launch/osep_results.launch.py
+++ src/osep/launch/osep_results.launch.py
@@ -11,23 +11,11 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='osep',
-            executable='tsdf_to_pointcloud_node',
-            name='tsdf_to_pointcloud_node',
-            parameters=[{
-                'output_topic': 'osep/tsdf/pointcloud',
-                'static_output_topic': 'osep/tsdf/static_pointcloud',
-                'cavity_fill_diameter': 5.0,
-                'voxel_size': VOXEL_SIZE
-            }],
-            output='screen'
-        ),
-        Node(
-            package='osep',
             executable='static_pointcloud_postprocessor_node',
             name='static_pointcloud_postprocessor_node',
             parameters=[{
                 'input_topic': 'osep/tsdf/static_pointcloud',
-                'output_topic': 'osep/tsdf/upsampled_static_pointcloud',
+                'output_topic': 'osep/tsdf/shell_static_pointcloud',
                 'voxel_size': VOXEL_SIZE,
                 'upsample_N': 1,
             }],
